4bbb33a353f866b2855c82a19fc80b449a59b86c
AlexBuck <underflux@googlemail.com>
2024-10-31 09:18:00 +0000

    some other changes

--- src/app/Retro-Photos/page.js
+++ src/app/Retro-Photos/page.js
@@ -6,13 +6,34 @@ import ThemeSelector from "../Components/themeSelector";
 export default function RetroPhotos() {
   return (
     <>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         <div>
           <img
             className="h-auto max-w-full rounded-lg"
             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
             alt=""
           />
+          {/* Open the modal using document.getElementById('ID').showModal() method */}
+          <button
+            className="btn"
+            onClick={() => document.getElementById("my_modal_1").showModal()}
+          >
+            open modal
+          </button>
+          <dialog id="my_modal_1" className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
         </div>
         {/* Open the modal using document.getElementById('ID').showModal() method */}
         <button
